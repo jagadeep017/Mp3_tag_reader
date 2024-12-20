ebfe32e3d3297c2702e81edc73e6bbebe4564d40
Jagadeep Ganta <152236029+jagadeep017@users.noreply.github.com>
2024-12-20 14:39:25 +0530

    added some extra print statements and fixed some edge cases

--- main.cpp
+++ main.cpp
@@ -6,15 +6,16 @@ using namespace std;
 int main(int argc,char **argv){
     int res=operation_type(argc,argv);  //getting the operation type
     mp3 obj;                            //creating object of mp3 class
-    if(res!=HELP){                      //if the operation type is not help
+    if(res!=HELP&&res!=NO_OPERATION){                      //if the operation type is not help
         strcpy(obj.name,argv[2]);
     }
     switch (res) {
     case NO_OPERATION:                  //if no operation is passed or invalid operation is passed
         cout<<"ERROR: INVALID ARGUMENT"<<endl;
         cout<<"USAGE:"<<endl;
-        cout<<"To view please pass like: ./a.out -v <.mp3>"<<endl;
-        cout<<"To edit please pass like: ./a.out -e -t/-a/-A/-m/-y/-c <.mp3>"<<endl;
+        cout<<"To get help or manual please pass like : ./a.out -h"<<endl;
+        cout<<"To view tags please pass like          : ./a.out -v <.mp3>"<<endl;
+        cout<<"To edit tags please pass like          : ./a.out -e -t/-a/-A/-m/-y/-c <.mp3>"<<endl;
         break;
     case HELP:                          //if help is passed
         help();
